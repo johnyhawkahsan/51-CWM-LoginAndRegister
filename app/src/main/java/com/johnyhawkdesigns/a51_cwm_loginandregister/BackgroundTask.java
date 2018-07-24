6de90bf5dd37e35ab681ef4738db3232d5139357
Ahsan <johnyhawkahsan@gmail.com>
2018-07-24 15:40:22 +0500

    3rd Commit

--- app/src/main/java/com/johnyhawkdesigns/a51_cwm_loginandregister/BackgroundTask.java
+++ app/src/main/java/com/johnyhawkdesigns/a51_cwm_loginandregister/BackgroundTask.java
@@ -1,6 +1,10 @@
 package com.johnyhawkdesigns.a51_cwm_loginandregister;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by Ahsan on 7/24/2018.
@@ -8,8 +12,29 @@ import android.os.AsyncTask;
 
 public class BackgroundTask extends AsyncTask<String, Void, String> {
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+
+    Context context;
+
+    BackgroundTask(Context context){
+        this.context = context;
+    }
+
     @Override
     protected String doInBackground(String... params) {
+
+        sharedPreferences = context.getSharedPreferences("MYPREFS", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.putString("flag", "0");
+        editor.commit();
+
+        String urlRegistration = "http://localhost/sql-connection/LoginAndRegister-register.php";
+        String urlLogin = "http://localhost/sql-connection/LoginAndRegister-login.php";
+
+        String task = params[0]; //Get params at 0 index
+
         return null;
     }
 
